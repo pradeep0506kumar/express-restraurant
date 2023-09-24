d2a22a405b714c22076578699f61222e4293d53d
unknown <Abhilash23kumar@gmail.com>
2023-09-24 11:06:40 +0530

    chnages

--- express-restraurant-UI/src/app/components/our-menu/our-menu.component.ts
+++ express-restraurant-UI/src/app/components/our-menu/our-menu.component.ts
@@ -6,5 +6,674 @@ import { Component } from '@angular/core';
   styleUrls: ['./our-menu.component.scss']
 })
 export class OurMenuComponent {
+  menuItems = [
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 2,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 1,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+
+    {
+      title: "Biryani",
+      price: "Rs 50",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/image/01.jpg',
+      type: 'other'
+
+    },
+
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burgurei",
+      price: "Rs 60",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 2,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chese Burger",
+      price: "Rs 60",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/08.jpg',
+      type: 'breakfast'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+
+    },
+    {
+      title: "Chiken Rice",
+      price: "Rs 70",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/05.jpg',
+      type: 'lunch'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "Motton Rice",
+      price: "Rs 80",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/03.jpg',
+      type: 'dinner'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 2,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 3,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 4,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+    {
+      title: "soft drink",
+      price: "Rs 40",
+      rating: 5,
+      discription: "Lorem ipsum dolor sit amet.Lorem ipsum dolor",
+      tableNo: '4',
+      imgPath: '../assets/images/our-menu/01.jpg',
+      type: 'drinks'
+    },
+  ]
+  filters: string = 'all'
+  filterMenuItems: any[] = [];
+
+  ngOnInit() {
+    this.filterMenuItems = this.menuItems;
+  }
+
+  filterMenu(filter: string) {
+    this.filters = filter;
+    if (this.filters === 'all') {
+      this.filterMenuItems = this.menuItems;
+    } else {
+      this.filterMenuItems = this.menuItems.filter(element => (element.type == this.filters));
+    }
+
+  }
+  addInCart(item:any){
+    alert(item.title + ' is add to your cart');
+
+  }
 
 }
